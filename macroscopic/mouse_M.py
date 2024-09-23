cd2c42c2b5267ac4875754ec7000dcb702b9b4d7
Amadeus Maes <amadeus.maes@gmail.com>
2024-09-23 11:15:37 -0700

    New exp

--- macroscopic/mouse_M.py
+++ macroscopic/mouse_M.py
@@ -26,9 +26,9 @@ class MouseMEnv(gym.Env):
 
         # Observations are dictionaries with the agent's and the target's location.
         # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
-        self.observation_space = spaces.Box(low= np.array([0,0,0,0,0]), 
-                                            high= np.array([1,3600,2400,1200,1]), 
-                                            shape=(5,), 
+        self.observation_space = spaces.Box(low= np.array([0,0,0,0,0,0]), 
+                                            high= np.array([1,3600,2400,1200,1,1]), 
+                                            shape=(6,), 
                                             dtype=np.float64)
 
 
@@ -60,7 +60,7 @@ class MouseMEnv(gym.Env):
 
     def _get_obs(self):
         #return np.hstack((self._agent_location,self._agent_time_vec,self._agent_food,self._agent_threat))
-        return np.hstack((self._agent_location,self._agent_time_vec,self._agent_threat))
+        return np.hstack((self._agent_location,self._agent_time_vec,self._agent_time,self._agent_threat))
     
 
     def reset(self,location=0, side=0, side_prev=0, time_vec=np.array([0,0,0]) ,time=0, food=0, threat=0, seed=None, options=None):    
