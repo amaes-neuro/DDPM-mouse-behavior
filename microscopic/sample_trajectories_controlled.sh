3e9022c5924d13258d412c81c68b9098b9733d44
Amadeus Maes <amaes@scripps.edu>
2024-09-25 18:04:56 -0700

    error fix

--- microscopic/sample_trajectories_controlled.sh
+++ microscopic/sample_trajectories_controlled.sh
@@ -16,8 +16,8 @@ for IDX in $(seq 0 $range_end); do
   (
   echo "Starting job #$((IDX+1)) of ${n} jobs."
   srun --ntasks=1 --nodes=1 --mem=4G --time=4:00:00 --gpus-per-task=1 --job-name="parallel_sampling" \
-  --output="out/par_$counter.out" --error="err/par_$counter.err" --partition="gpu" --exclusive -c 1 \
-  python synthetic_probs_gen_par.py $"t_4" $"balanced4" "$IDX"
+  --output="out/par_$counter.out" --error="err/par_$counter.err" --partition=gpu \
+  python synthetic_probs_gen_par.py $"t_4" $"balanced4" $IDX
   ) &
 
   # batch control
